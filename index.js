fae4c10e2bd4b280e94982e803bd1095c2fd15fe
KimEunA97 <kimeuna9712@gmail.com>
2023-03-20 10:22:11 +0900

    모듈

---
+++ index.js
@@ -0,0 +1,5 @@
+//common js 방식
+// const fromModuleObjectBasicData = require('./module/basic-data.js');
+// import export 모듈 방식
+import fromModuleObjectBasicData from './module/basic-data.js';
+console.dir(fromModuleObjectBasicData);
---
+++ module/basic-data.js
@@ -0,0 +1,5 @@
+const a = {
+  a : "이것은 a 데이터여",
+}
+
+export default a;
