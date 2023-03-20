ef93b16d0ed33938c7bb006a477ec8fc82c0d5b2
KimEunA97 <kimeuna9712@gmail.com>
2023-03-20 11:58:27 +0900

    서버 통신으로 html 만들기

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 import http from 'http';
 console.dir(http);
-const server = http.createServer();
+
+//콜백원리와 같다.
+const server = http.createServer(function (request, response) {
+
+  //만들어줘야 됨 
+  //비즈니스 로직
+  let body = "";
+  body += "<!DOCTYPE html>";
+  body += "<html>";
+  body += "<head>";
+  body += "<title>Node.js</title>";
+  body += "</head>";
+  body += "<body>";
+  body += "<h1>Hello World</h1>";
+  body += "</body>";
+  body += "</html>";
+
+  
+  //맞춰줘야 됨
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('hellow world');
+
+});
