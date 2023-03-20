18b65fbb4fc79e7406f8e1dcc5d4428b37873575
KimEunA97 <kimeuna9712@gmail.com>
2023-03-20 11:26:33 +0900

    콜백 연산

--- callback-test.js
+++ callback-test.js
@@ -1,23 +1,25 @@
-function six(number, append) {
+function plus(number, append) {
   return number + append;
 }
 
-function seven(number, append) {
+function minus(number, append) {
   return number - append;
 }
 
-function eight(number, append, callback) {
-  let a = number + 1;
-  let b = append + 2;
-  //인자를 전달 받는다.
-  //argument를 전달 받았다.
-  //매개변수 자리에 함수가 들어오는 것을 콜백함수라고 한다.
+function divide(number, append) {
+
+  return number / append;
+
+}
+function remain(number, append) {
+  return number % append;
+}
+
+function operator(number, append, callback) {
+  let a = number;
+  let b = append;
   return callback(a, b);
 
 }
 
-console.log(eight(1, 2, function (a, b) {
-  return a + b;
-}));
-
-// console.log(eight(3, 4, seven));
+console.log(operator(1504, 209, remain));
